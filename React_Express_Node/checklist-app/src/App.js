cd58a085fde6bfb7739399825927997260c74148
joanita-51 <nakityojoanita51@gmail.com>
2023-07-08 14:54:13 +0300

    Frontend design using react

--- React_Express_Node/checklist-app/src/App.js
+++ React_Express_Node/checklist-app/src/App.js
@@ -13,6 +13,7 @@ function App() {
     fetchItems();
   }, []);
 
+  //Read (CReadUD)
   //an async function waits for a hand check from a promise
   // used the axios library to get a request to the nodejs our backend and collect the response
   const fetchItems = async()=>{
@@ -23,14 +24,14 @@ function App() {
       // set the items to whatever we get from that server
       setItems(response.data);
     } catch (error) {
-      console.log("Error fetching items:", error)
+      console.error("Error fetching items:", error)
     }
   }
 
-  //CreateRUD
+  //Create (A way to add new entries to the database and display them on the screen)
   const addItem = async () =>{
     try{
-      // inside the post we pass in the route and the dictionary object which has the new item that we want to send to the backend
+      // inside the post we pass in the route and the dictionary object which has the new item that we want to send to the backend (Both send and receive data)
       const response = await axios.post('http://localhost:3001/items', {item:newItem}); 
       const newItemObj = {id:response.data.id, item:newItem};
       setItems([...items, newItemObj])
@@ -42,7 +43,7 @@ function App() {
     }
   }
 
-  // Update
+  // Update (with post , we not only send information to the server but we also receive it back)
   const editItem = async (id, updatedItem)=>{
     try {
       await axios.put(`http://localhost:3001/items/${id}`, {item:updatedItem});
@@ -58,9 +59,12 @@ function App() {
   //Delete
   //The asynchronous function is helping us in a way that we need the nodejs server to respond and this can take sometime
   //The await helps us to tell js that it should not move on to setItems until it hears back from the nodejs server
+  //This time we are not passing any new information to the server, we are just sending the request to the path and the endpoint will execute on the server.
   const deleteItem = async (id)=>{
     try{
       await axios.delete(`http://localhost:3001/items/${id}`);
+      // updating the values on our frontend
+      // filter the items so that we can get a list of every item such that it is not the same as the one we want to delete
       setItems(items.filter((item)=> item.id !== id));
     } catch(error){
       console.error('Error deleting item:', error)
@@ -68,21 +72,34 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hacker Planner</h1>
+      <ul className='checklist-items'>
+        {/* The map maps every single item inside our list database to its own list element  */}
+        {items.map((item)=>(
+          <li key={item.id}>
+            <span>{item.item}</span>
+            <button className='remind-button'
+              onClick={()=> editItem(item.id, prompt('Enter updated item',item.item))}
+            >
+              Edit
+            </button>
+            <button onClick={()=>deleteItem(item.id)}>
+              Delete
+            </button>
+            <button onClick={()=>setReminder(item.id)}>
+              Remind
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      {/* Used to add items */}
+      <div className='add-form'>
+          <input type="text" value={newItem} onChange={(e)=> setNewItem(e.target.value)}/>
+          <button onClick={addItem}>Add</button>
+      </div>
+
     </div>
   );
 }
